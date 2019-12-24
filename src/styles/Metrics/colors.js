13da791c18be17838beff47d60b5991994ed8f25
Alex Madeira <alex.c.madeira@gmail.com>
2019-12-24 12:03:02 +0000

    colors.js alterados online com o Bitbucket

--- src/styles/Metrics/colors.js
+++ src/styles/Metrics/colors.js
@@ -1,10 +1,20 @@
 const Colors = {
   White: '#FFFFFF',
-  Light: '#f7f7f7',
+  Light: '#F1F1F1',
   LightShadow: 'rgba(255, 255, 255, 0.35)',
   DarkShadow: 'rgba(0, 0, 0, 0.15)',
   DarkHover: '#1a1d21',
   Shadow: 'rgba(0, 0, 0, 0.8)',
   Black: '#000000',
+  TextColor: {
+    Dark: '#554F49',
+    Light: '#FFFFFF',
+    highlighted: '#EAC591',
+  },
+  Backgrounds: {
+    Banner: '#C2AA94',
+    Destaque: '#83aa73',
+    Clean: '#f0e0b7',
+  },
 };
 export default Colors;
